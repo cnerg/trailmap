f41b7f20122589648dfb40ea73b1b6dbd0da9fd5
Katie Mummah <radioactivekate@gmail.com>
2021-03-17 04:16:59 -0600

    simplify commodity dictionary and add testing (#37)

--- scripts/main.py
+++ scripts/main.py
@@ -17,6 +17,8 @@ def make_parser():
                    default='trailmap.gpickle',
                    help='output path for pickled graph')
     p.add_argument('--supress-print', '-sp', action='store_true')
+    p.add_argument('--metadata', '-m', nargs=1, help='Cyclus metadata file',
+                   type=argparse.FileType('r'))
 
     return p
 
@@ -26,7 +28,12 @@ def main(args=None):
     p = make_parser()
     ns = p.parse_args(args=args)
 
-    commodity_dictionary = cd.build_commod_dictionary()
+    if ns.metadata:
+        m = ns.metadata[0]
+        print(m["specs"])
+        commodity_dictionary = cd.build_commod_dictionary(ns.metadata[0])
+    else:
+        commodity_dictionary = cd.build_commod_dictionary()
     (facility_dict_in,
     facility_dict_out) = pi.parse_input(ns.infile[0], commodity_dictionary)
     (G, pathways) = ap.conduct_apa(facility_dict_in, facility_dict_out)
--- tests/test_commodity_dictionary.py
+++ tests/test_commodity_dictionary.py
@@ -22,3 +22,41 @@ def test_commod_names_outcomodity():
     assert ['fuel_outcommods'] == cd.get_commod_names(metadata["annotations"],
                                                       "outcommodity",
                                                       ":cycamore:Reactor")
+
+
+def test_build_facility_dictionary():
+    exp = {':agents:KFacility': (['in_commod'], ['out_commod']),
+           ':agents:NullInst': ([], []),
+           ':agents:NullRegion': ([], []),
+           ':agents:Predator': (['prey'], ['commod']),
+           ':agents:Prey': ([], ['commod']),
+           ':agents:Sink': (['in_commods'], []),
+           ':agents:Source': ([], ['commod']),
+           ':cycamore:DeployInst': ([], []),
+           ':cycamore:Enrichment': (['feed_commod'], ['product_commod',
+                                     'tails_commod']),
+           ':cycamore:FuelFab': (['fill_commods', 'fiss_commods',
+                                  'topup_commod'], ['outcommod']),
+           ':cycamore:GrowthRegion': ([], []),
+           ':cycamore:ManagerInst': ([], []),
+           ':cycamore:Mixer': ([], ['out_commod']),
+           ':cycamore:Reactor': (['fuel_incommods', 'pref_change_commods',
+                                  'recipe_change_commods'],
+                                 ['fuel_outcommods']),
+           ':cycamore:Separations': (['feed_commods'], ['leftover_commod',
+                                      'streams_']),
+           ':cycamore:Sink': (['in_commods'], []),
+           ':cycamore:Source': ([], ['outcommod']),
+           ':cycamore:Storage': (['in_commods'], ['out_commods'])}
+
+    assert cd.build_facility_dictionary(metadata, specs) == exp
+
+
+def test_build_facility_dictionary_subset():
+    archetypes = [':cycamore:Sink', ':cycamore:Source', ':cycamore:Storage']
+    exp = {':cycamore:Sink': (['in_commods'], []), 
+           ':cycamore:Source': ([], ['outcommod']),
+            ':cycamore:Storage': (['in_commods'], ['out_commods'])}
+
+    assert cd.build_facility_dictionary(metadata, archetypes) == exp
+    

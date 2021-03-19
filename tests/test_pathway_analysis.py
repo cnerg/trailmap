90ae7f5ecbfc3faec6b6a68dee29fb59ccd9b01a
Katie Mummah <radioactivekate@gmail.com>
2021-03-19 11:59:37 -0600

    Analyze pathways (#34)
    * moving from AP branch
    
    * test updates
    
    * add more complex cycle to test suite
    
    * revising PA fuctions and adding tests
    
    * roll loops to get proper pathways with cycles
    
    * more tests
    
    * fix formatting of cycles and add more tests
    
    * find flow for single pathway
    
    * clean up docstrings
    
    * prints graph parameters and returns G, pathways
    
    * Update trailmap/pathway_analysis.py
    
    Co-authored-by: Paul Wilson <paul.wilson@wisc.edu>
    
    * Update trailmap/pathway_analysis.py
    
    Co-authored-by: Paul Wilson <paul.wilson@wisc.edu>
    
    * Apply suggestions from code review
    
    Co-authored-by: Paul Wilson <paul.wilson@wisc.edu>
    
    * addressing review comments
    
    * addressing PR comments
    
    * fix flow stuff
    
    * update tests and add parametrization where reasonable
    
    * Apply suggestions from code review
    
    Co-authored-by: Paul Wilson <paul.wilson@wisc.edu>
    
    * change how ndp handles being passed bad nodes
    
    * making sure tests cover recent code modifications
    
    * Apply suggestions from code review
    
    Co-authored-by: Paul Wilson <paul.wilson@wisc.edu>
    
    * simplify commodity dictionary and add testing (#37)
    
    * addressing testing comments
    
    Co-authored-by: Paul Wilson <paul.wilson@wisc.edu>

---
+++ tests/test_pathway_analysis.py
@@ -0,0 +1,554 @@
+import io
+import sys
+import trailmap.pathway_analysis as pa
+import networkx as nx
+import pytest
+from tests.pa_data import testdata
+import collections
+from math import inf
+# name, short, long, semiconnect, hierarchy, edges, paths, sc
+
+
+def test_ndp():
+    exp = {('mine', 'civ_enrich', 'heu_pu_collector'),
+           ('mine', 'mil_enrich', 'heu_pu_collector')}
+
+    G = nx.DiGraph()
+    edges = [('mine', 'civ_enrich'), ('mine', 'mil_enrich'),
+             ('civ_enrich', 'heu_pu_collector'), ('civ_enrich', 'reactor'),
+             ('reactor', 'reprocess'), ('reprocess', 'heu_pu_collector'),
+             ('mil_enrich', 'heu_pu_collector')]
+    G.add_edges_from(edges)
+
+    obs = pa.find_node_disjoint_paths(G, 'mine', 'heu_pu_collector')
+
+    assert obs == exp
+
+
+@pytest.mark.parametrize("source,target", [('import', 'heu_pu_collector'),
+                                        ('mine', 'export'),
+                                        ('import', 'export')])
+def test_ndp_raise_exception(source, target):
+    G = nx.DiGraph()
+    edges = [('mine', 'civ_enrich'), ('mine', 'mil_enrich'),
+             ('civ_enrich', 'heu_pu_collector'), ('civ_enrich', 'reactor'),
+             ('reactor', 'reprocess'), ('reprocess', 'heu_pu_collector'),
+             ('mil_enrich', 'heu_pu_collector')]
+    G.add_edges_from(edges)
+
+    with pytest.raises(ValueError) as excinfo:   
+        obj = pa.find_node_disjoint_paths(G, source, target)
+        assert 'Source and/or target not in graph G!' in str(excinfo.value)
+
+
+def test_is_multidigraph_true():
+    exp = True
+    G = nx.MultiDiGraph()
+    G.add_edge('a', 'b')
+    obs = pa.is_multidigraph(G)
+
+    assert obs == exp
+
+
+def test_is_multidigraph_graph():
+    exp = False
+    G = nx.Graph()
+    G.add_edge('a', 'b')
+    obs = pa.is_multidigraph(G)
+
+    assert obs == exp
+
+
+def test_is_multidigraph_digraph():
+    exp = False
+    G = nx.DiGraph()
+    G.add_edge('a', 'b')
+    obs = pa.is_multidigraph(G)
+
+    assert obs == exp
+
+
+def test_is_multidigraph_multigraph():
+    exp = False
+    G = nx.MultiGraph()
+    G.add_edge('a', 'b')
+    obs = pa.is_multidigraph(G)
+
+    assert obs == exp
+
+
+def test_has_multiedges_true():
+    exp = True
+    G = nx.MultiDiGraph()
+    G.add_edge('a', 'b')
+    G.add_edge('a', 'b')
+
+    obs = pa.has_multiedges(G)
+    assert obs == exp
+
+
+def test_has_multiedges_false():
+    exp = False
+    G = nx.MultiDiGraph()
+    G.add_edge('a', 'b')
+    G.add_edge('a', 'c')
+
+    obs = pa.has_multiedges(G)
+    assert obs == exp
+
+
+def test_has_multiedges_not_multigraph():
+    exp = None
+    G = nx.DiGraph()
+    G.add_edge('a', 'b')
+    
+    obs = pa.has_multiedges(G)
+    assert obs == exp
+
+
+def test_transform_to_digraph():
+    G = nx.MultiDiGraph()
+    G.add_edge('a', 'b')
+    exp_H = nx.DiGraph()
+    exp_H.add_edge('a', 'b')
+    exp = list(exp_H.edges())
+
+    (obs_H, obs_safe) = pa.transform_to_digraph(G)
+    obs = list(obs_H.edges())
+
+    assert obs == exp
+
+
+@pytest.mark.parametrize("G", [(nx.Graph()), (nx.MultiGraph())])
+def test_transform_to_digraph_unsafe(G):
+    exp = False
+    G.add_edge('a', 'b')
+    G.add_edge('a', 'b')
+
+    (obs_H, obs_safe) = pa.transform_to_digraph(G)
+
+    assert obs_safe == exp
+
+
+@pytest.mark.parametrize("G,exp", [(nx.Graph(), None), 
+                                   (nx.MultiGraph(), None)])
+def test_transform_to_digraph_formats(G, exp):
+    G.add_edge('a', 'b')
+    (obs_H, obs_safe) = pa.transform_to_digraph(G)
+
+    assert obs_H == exp
+
+
+@pytest.mark.parametrize("G,exp", [(nx.Graph(), False), 
+                                   (nx.DiGraph(), True),
+                                   (nx.MultiGraph(), False),
+                                   (nx.MultiDiGraph(), True)])
+def test_transform_to_digraph_formats_is_safe(G, exp):
+    G.add_edge('a', 'b')
+    (obs_H, obs_safe) = pa.transform_to_digraph(G)
+
+    assert obs_safe == exp
+
+
+def test_maximum_flow_simple():
+    exp = 3
+    G = nx.DiGraph()
+    G.add_edge('a', 'b', capacity=3)
+    G.add_edge('b', 'c', capacity=4)
+    G.add_edge('c', 'd', capacity=3)
+    G.add_edge('b', 'd', capacity=10)
+    (obs_path, obs) = pa.find_maximum_flow(G, 'a', 'd')
+
+    assert obs == exp
+
+
+def test_maximum_flow_several_paths():
+    exp = 3
+    G = nx.DiGraph()
+    G.add_edge('a', 'd', capacity=1)
+    G.add_edge('a', 'b', capacity=1)
+    G.add_edge('a', 'c', capacity=1)
+    G.add_edge('b', 'd', capacity=2)
+    G.add_edge('c', 'd', capacity=5)
+    (obs_path, obs) = pa.find_maximum_flow(G, 'a', 'd')
+
+    assert obs == exp
+
+
+def test_maximum_flow_multigraph():
+    '''NetworkX retains only the most recently added edge if multiple edges 
+    exit and a MultiGraph is converted into a DiGraph'''
+    exp = 1
+    G = nx.DiGraph()
+    G.add_edge('a', 'b', capacity=2)
+    G.add_edge('a', 'b', capacity=1)
+    G.add_edge('b', 'c', capacity=5)
+
+    (obs_path, obs) = pa.find_maximum_flow(G, 'a', 'c')
+
+    assert obs == exp
+
+
+def test_maximum_flow_multigraph_order_flipped():
+    '''NetworkX retains only the most recently added edge if multiple edges 
+    exit and a MultiGraph is converted into a DiGraph'''
+    exp = 2
+    G = nx.DiGraph()
+    G.add_edge('a', 'b', capacity=1)
+    G.add_edge('a', 'b', capacity=2)
+    G.add_edge('b', 'c', capacity=5)
+
+    (obs_path, obs) = pa.find_maximum_flow(G, 'a', 'c')
+
+    assert obs == exp
+
+
+def test_maximum_flow_directed_only():
+    exp = 5
+    G = nx.DiGraph()
+    G.add_edge('a', 'b', capacity=4)
+    G.add_edge('a', 'c', capacity=4)
+    G.add_edge('b', 'd', capacity=2)
+    G.add_edge('b', 'e', capacity=1)
+    G.add_edge('c', 'd', capacity=3)
+    G.add_edge('d', 'e', capacity=4)
+
+    (obs_path, obs) = pa.find_maximum_flow(G, 'a', 'e')
+
+    assert obs == exp
+
+
+def test_maximum_flow_not_directed_or_multi():
+    exp = None
+    G = nx.Graph()
+    G.add_edge('a', 'b', capacity = 2)
+    G.add_edge('b', 'c', capacity = 5)
+
+    with pytest.raises(TypeError) as excinfo:
+        (obs_path, obs) = pa.find_maximum_flow(G, 'a', 'c')
+        assert 'Graph must be DiGraph type' in str(excinfo)
+
+
+def test_find_pathway_flow():
+    G = nx.DiGraph()
+    edges = [(0, 1, {'capacity': 3}),
+           (1, 2, {'capacity': 5}),
+           (2, 3, {'capacity': 3}),
+           (3, 4, {'capacity': 4}),
+           (0, 5, {'capacity': 2}),
+           (5, 6, {'capacity': 3}),
+           (6, 3, {'capacity': 2}),
+           (3, 7, {'capacity': 1}),
+           (7, 4, {'capacity': 2})]
+    G.add_edges_from(edges)
+    path = (0, 5, 6, 3, 7)
+
+    exp = 1
+    obs = pa.find_pathway_flow(G, path)
+
+    assert obs == exp
+
+def test_find_pathway_flow_no_capacity():
+    G = nx.DiGraph()
+    edges = [(0,1), (1,2), (2,3)]
+    G.add_edges_from(edges)
+    path = (0, 1, 2, 3)
+
+    with pytest.raises(nx.exception.NetworkXUnbounded) as excinfo:   
+        obj = pa.find_pathway_flow(G, path) 
+        assert 'Infinite capacity path, flow unbounded above.' in str(excinfo.value)
+
+
+def test_find_pathway_flow_single_infinite():
+    G = nx.DiGraph()
+    edges = [(0, 1, {'capacity': 3}),
+             (1, 2, {'capacity': inf}),
+             (2, 3, {'capacity': 3})]
+    G.add_edges_from(edges)
+    path = (0, 1, 2, 3)
+    obs = pa.find_pathway_flow(G, path)
+
+    exp = 3
+
+    assert obs == exp
+
+
+def test_find_pathway_flow_all_infinite():
+    G = nx.DiGraph()
+    edges = [(0, 1, {'capacity': inf}),
+             (1, 2, {'capacity': inf}),
+             (2, 3, {'capacity': inf})]
+    G.add_edges_from(edges)
+    path = (0, 1, 2, 3)
+
+    with pytest.raises(nx.exception.NetworkXUnbounded) as excinfo:   
+        obj = pa.find_pathway_flow(G, path) 
+        assert 'Infinite capacity path, flow unbounded above.' in str(excinfo.value)
+
+
+def test_find_pathway_flow_multiedges():
+    G = nx.MultiDiGraph()
+    edges = [(0,1), (1,2), (1,2), (2,3)]
+    G.add_edges_from(edges)
+    pathway = (0, 1, 2, 3)
+
+    with pytest.raises(TypeError) as excinfo:
+        obs = pa.find_pathway_flow(G, pathway)
+        assert 'Graph must be DiGraph type. Use' in str(excinfo)
+
+
+def test_find_pathway_flow_other_type():
+    G = nx.Graph()
+    edges = [(0,1), (1,2), (2,3)]
+    G.add_edges_from(edges)
+    pathway = (0, 1, 2, 3)
+
+    with pytest.raises(TypeError) as excinfo:
+        obs = pa.find_pathway_flow(G, pathway)
+        assert 'Graph must be DiGraph type' in str(excinfo)
+
+
+@pytest.mark.parametrize("steps,exp", [(('FacilityA', 'FacilityB'), 1),
+                                       (('FacilityA', 'Sink'), -1)])
+def test_check_if_sublist(steps,exp):
+    path = ('Source', 'FacilityA', 'FacilityB', 'Sink')
+    pos = pa.check_if_sublist(path, steps)
+    assert pos == exp
+
+
+@pytest.mark.parametrize("cycle,exp", [([3, 4, 7], (7, 3, 4)),
+                                       ([7, 3, 4], (7, 3, 4)),
+                                       ([5, 6], None),
+                                       ([6], None),
+                                       ([], None)])
+def test_roll_cycle(cycle, exp):
+    path = (0, 1, 3, 4, 7, 8)
+
+    obs = pa.roll_cycle(path, cycle)
+    assert obs == exp
+
+
+def test_insert_cycle_single():
+    path = (0, 1, 3, 4, 7, 8)
+    rolled_cycles = {(7, 3, 4)}
+    
+    exp = (0, 1, 3, 4, (7, 3, 4), 7, 8)
+    obs = pa.insert_cycles(path, rolled_cycles)
+    assert obs == exp
+
+
+def test_insert_cycle_loop():
+    path = (0, 1, 3, 4, 7, 8)
+    rolled_cycles = {(7,)}
+    
+    exp = (0, 1, 3, 4, (7,), 7, 8)
+    obs = pa.insert_cycles(path, rolled_cycles)
+    assert obs == exp
+
+
+def test_insert_cycle_multiple():
+    path = (0, 2, 6, 7, 8)
+    rolled_cycles = ((6, 5), (7, 3, 4))
+
+    exp = (0, 2, (6, 5), 6, (7, 3, 4), 7, 8)
+    obs = pa.insert_cycles(path, rolled_cycles)
+    assert obs == exp
+
+
+def test_insert_cycle_multiple_same_index():
+    path = ('Source' , 'A', 'B', 'C', 'Sink')
+    rolled_cycles = (('C', 'D', 'B'), ('C', 'D', 'E', 'A', 'B'))
+    
+    exp = ('Source' , 'A', 'B', ('C', 'D', 'B'), ('C', 'D', 'E', 'A', 'B'),
+           'C', 'Sink')
+    obs = pa.insert_cycles(path, rolled_cycles)
+    assert obs == exp
+
+
+def test_insert_cycle_multiple_same_index_with_loop():
+    path = (0, 2, 6, 7, 8)
+    rolled_cycles = {(7,), (7, 3, 4)}
+
+    exp = (0, 2, 6, (7,), (7, 3, 4), 7, 8)
+    obs = pa.insert_cycles(path, rolled_cycles)
+    assert obs == exp
+
+
+def test_get_pathways_with_cycles():
+    pathways = {(0, 1, 3, 4, 7, 8)}
+    sc = [[2, 1]]
+
+    exp = {(0, (1, 2), 1, 3, 4, 7, 8)}
+    obs = pa.get_pathways_with_cycles(pathways, sc)
+
+    assert obs == exp
+
+
+def test_get_pathways_with_cycles_multiple():
+    pathways = {(0, 1, 3, 4, 7, 8), (0, 2, 3, 4, 7, 8), (0, 2, 6, 7, 8)}
+    sc = [[5, 6], [3, 4, 7]]
+
+    exp = {(0, 1, 3, 4, (7, 3, 4), 7, 8),
+           (0, 2, 3, 4, (7, 3, 4), 7, 8),
+           (0, 2, (6, 5), 6, (7, 3, 4), 7, 8)}
+
+    obs = pa.get_pathways_with_cycles(pathways, sc)
+    #assert all(x in obs for x in exp)
+    assert obs == exp
+
+
+def test_get_pathways_with_cycles_loop():
+    pathways = {(0, 1, 3, 4, 7, 8), (0, 2, 3, 4, 7, 8), (0, 2, 6, 7, 8)}
+    sc = [[7], [3, 4, 7]]
+
+    exp = {(0, 1, 3, 4, (7, 3, 4), (7,), 7, 8),
+           (0, 2, 3, 4, (7, 3, 4), (7,), 7, 8),
+           (0, 2, 6, (7, 3, 4), (7,), 7, 8)}
+
+    obs = pa.get_pathways_with_cycles(pathways, sc)
+    assert obs == exp
+
+
+def test_get_pathways_with_cycles_none():
+    pathways = []
+    obs = pa.get_pathways_with_cycles(pathways, [[2, 1]])
+
+    assert obs == set()
+
+
+def data():
+    return testdata
+
+
+@pytest.mark.parametrize("source,exp", [("SourceA",
+                                        {("SourceA", "Facility", "SinkA"),
+                                        ("SourceA", "Facility", "SinkB")}),
+                                        ("SourceC", set()),
+                                        ("Facility", set())])
+def test_find_paths_with_source(source,exp):
+    pathways = testdata[2][4]
+
+    obs_subset = pa.find_paths_with_source(pathways, source)
+    assert obs_subset == exp
+
+
+def test_find_paths_with_source_none():
+    pathways = []
+    obs = pa.find_paths_with_source(pathways, "SourceA")
+
+    assert obs == set()
+
+
+@pytest.mark.parametrize("sink,exp", [("SinkB",
+                                      {("SourceA", "Facility", "SinkB"),
+                                      ("SourceB", "Facility", "SinkB")}),
+                                      ("SinkC", set()),
+                                      ("SourceA", set())])
+def test_find_paths_with_sink(sink,exp):
+    pathways = testdata[2][4]
+
+    obs_subset = pa.find_paths_with_sink(pathways, sink)
+    assert obs_subset == exp
+
+
+def test_find_paths_with_sink_none():
+    pathways = []
+    obs = pa.find_paths_with_sink(pathways, "SinkA")
+
+    assert obs == set()
+
+
+@pytest.mark.parametrize("contain,exp", [("SourceB", 
+                                         {("SourceB", "Facility", "SinkA"),
+                                         ("SourceB", "Facility", "SinkB")}),
+                                         (["SourceB", "SinkB"],
+                                         {("SourceB", "Facility", "SinkB")}),
+                                         (["SourceB", "SourceA"], set()),
+                                         ([], set())])
+def test_find_paths_containing_all(contain, exp):
+    pathways = testdata[2][4]
+
+    obs = pa.find_paths_containing_all(pathways, contain)
+    assert obs == exp
+
+
+@pytest.mark.parametrize("contain,exp", [(5, {(0,1,5,6,4)}),
+                                         ([5], {(0,1,5,6,4)}),
+                                         ([1, 5], {(0,1,5,6,4)}),
+                                         ([0, 3], {(0,1,2,3,4), (0,2,3,4)}),
+                                         ([5, 7], set()),
+                                         ([8], set()),
+                                         ([7,8], set()),
+                                         ([], set())])
+def test_find_paths_containing_all_int(contain, exp):
+    pathways = {(0,1,2,3,4), (0,2,3,4), (0,1,5,6,4), (0,1,7,4)}
+    
+    obs = pa.find_paths_containing_all(pathways, contain)
+    assert obs == exp
+
+
+def test_find_paths_containing_all_none():
+    pathways = []
+    obs = pa.find_paths_containing_all(pathways, "SinkA")
+
+    assert obs == set()
+
+
+@pytest.mark.parametrize("contain,exp", [("SourceA",
+                                         {("SourceA", "Facility", "SinkA"),
+                                         ("SourceA", "Facility", "SinkB")}),
+                                         (["SourceA", "SinkA"],
+                                         {("SourceA", "Facility", "SinkA"),
+                                         ("SourceA", "Facility", "SinkB"),
+                                         ("SourceB", "Facility", "SinkA")}),
+                                         (["SourceC"], set()),
+                                         ([], set())])
+def test_find_paths_containing_one_of(contain, exp):
+    pathways = testdata[2][4]
+
+    obs = pa.find_paths_containing_one_of(pathways, contain)
+    assert obs == exp
+
+
+@pytest.mark.parametrize("contain,exp", [(5, {(0,1,5,6,4)}),
+                                         ([5], {(0,1,5,6,4)}),
+                                         ([5, 7], {(0,1,5,6,4),(0,1,7,4)}),
+                                         (8, set()),
+                                         ([8], set()),
+                                         ([7,8], {(0,1,7,4)}),
+                                         ([], set())])
+def test_find_paths_containing_one_of_int(contain, exp):
+    pathways = {(0,1,2,3,4), (0,2,3,4),(0,1,5,6,4), (0,1,7,4)}
+
+    obs = pa.find_paths_containing_one_of(pathways, contain)
+    assert obs == exp
+
+
+def test_get_shortest_path_length_none():
+    pathways = {}
+    exp = set()
+    obs = pa.get_shortest_path(pathways)
+
+    assert obs == exp
+
+
+@pytest.mark.parametrize("name, short, long, edges, paths, sc", testdata)
+def test_get_shortest_paths(name, short, long, edges, paths, sc):
+    exp = {path for path in paths if len(path) == short}
+    obs = pa.get_shortest_path(paths)
+    assert obs == exp
+
+
+def test_get_longest_path_length_none():
+    pathways = {}
+    exp = set()
+    obs = pa.get_longest_path(pathways)
+
+    assert obs == exp
+
+
+@pytest.mark.parametrize("name, short, long, edges, paths, sc", testdata)
+def test_get_longest_paths(name, short, long, edges, paths, sc):
+    exp = {path for path in paths if len(path) == long}
+    obs = pa.get_longest_path(paths)
+    assert obs == exp
---
+++ trailmap/pathway_analysis.py
@@ -0,0 +1,294 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+from more_itertools import pairwise
+from collections import Counter
+
+
+def print_graph_parameters(G, pathways): # pragma: no cover
+    '''Prints a set of parameters characterizing the graph
+    '''
+    print('\nGRAPH PARAMETERS')
+
+    num_paths = len(pathways)
+    print("A total of " + str(num_paths) + " pathways were generated")
+
+    shortest = get_shortest_path(pathways)
+
+    longest = get_longest_path(pathways)
+
+    print("\nThe shortest pathway is length " + str(shortest_length))
+    print("pathways with this length are " + str(len(shortest.pop())))
+
+    print("\nGraph depth is " + str(len(longest.pop())))
+    print("pathways with this length are " + str(longest))
+
+    semiconnected = nx.is_semiconnected(G)
+    print('\nIs the graph semiconnected? ' + str(semiconnected))
+    if semiconnected is False:
+        if len(list(n for n, in_deg in G.in_degree() if in_deg == 0)) > 1:
+            print("You have multiple source facilities")
+
+    hierarchy = nx.flow_hierarchy(G)
+    print("\nGraph hierarchy is " + "{:.3f}".format(hierarchy))
+
+    return
+
+
+def find_node_disjoint_paths(G, s, t):
+    '''Returns a set of paths that share a source and target node, but have
+    exactly 0 shared edges.
+    '''
+    ndp = set()
+    if s in G and t in G:
+        paths = list(nx.node_disjoint_paths(G, s, t))
+        [ndp.add(tuple(path)) for path in paths]
+    else:
+        raise ValueError('Source and/or target not in graph G!')
+
+    return ndp
+
+
+def is_multidigraph(G):
+    return (G.is_directed() and G.is_multigraph())
+
+
+def has_multiedges(G):
+    '''Determines if graph G contains multiple edges between any pair of
+    nodes. Returns True, False, or None if the provided graph is not a
+    NetworkX Multigraph.
+    '''
+    if is_multidigraph(G):
+        H = nx.DiGraph(G)
+        return (sorted(H.edges()) != sorted(G.edges()))
+    else:
+        return None
+
+
+def transform_to_digraph(G):
+    '''Reduces multigraph to digraph and returns whether the transform is
+    safe/does not lose edges (True), or if the the transform is unsafe
+    and information is lost (False)
+    '''
+    if is_multidigraph(G):
+        if has_multiedges(G) is False:
+            safe = True
+        else:
+            safe = False
+        H = nx.DiGraph(G)
+        return H, safe
+    elif G.is_directed():
+        return G, True
+    else:
+        return None, False
+
+
+def find_maximum_flow(H, s, t):
+    '''Finds maximum flow between a source and target node in DiGraph G.
+    Requires edge attribute 'capacity'. MultiDiGraphs not supported.
+    '''    
+    if type(H) == nx.classes.digraph.DiGraph:
+        max_flow_path = nx.maximum_flow(H, s, t)
+        max_flow = nx.maximum_flow_value(H, s, t)
+        return max_flow_path, max_flow
+    else:
+        raise TypeError('Graph must be DiGraph type. Use \
+            convert_to_digraph to help convert a MultiDiGraph to a DiGraph, \
+            which may result in loss of information')
+
+
+def find_pathway_flow(H, pathway):
+    '''returns the maximum permissible flow for a given pathway in DiGraph G. 
+    Any edge without 'capacity' attribute will be given infinite capacity.
+    MultiDiGraphs not supported.
+    '''
+    if type(H) == nx.classes.digraph.DiGraph:
+        edges = set(pairwise(pathway))
+        H_sg = H.edge_subgraph(edges).copy()
+
+        sources = get_sources(H_sg)
+        sinks = get_sinks(H_sg)
+
+        path_flow = nx.maximum_flow_value(H_sg, sources[0], sinks[0])
+            
+        return path_flow
+
+    elif type(H) == nx.classes.multidigraph.MultiDiGraph:
+        raise TypeError('Graph must be DiGraph type. Use \
+            convert_to_digraph to help convert a MultiDiGraph to a DiGraph, \
+            which may result in loss of information')
+    else:
+        raise TypeError('Graph must be DiGraph type.')
+
+
+def find_simple_cycles(G): # pragma: no cover
+    '''finds cycles in a graph and returns them in a list of lists.
+    '''
+    sc = list(nx.simple_cycles(G))
+    return sc
+
+
+def check_if_sublist(path, list_of_steps):
+    '''Checks to see if a pathway contains each element in steps (list)
+    in order. Returns the position where the steps begin. If False, returns
+    position -1.
+    '''
+    pos = -1
+
+    if len(path) is 0 or len(list_of_steps) is 0:
+        return pos
+
+    for i in range(len(path) - len(list_of_steps)+1):
+        if path[i] == list_of_steps[0]:
+            n = 1
+            while (n < len(list_of_steps) and path[i+n] == list_of_steps[n]):
+                n+=1
+            if n == len(list_of_steps):
+                pos = i
+                break
+
+    return pos
+
+
+def roll_cycle(path, cycle):
+    '''Checks if a cycle can be entered from a node in a simple path. If so,
+    finds the last overlapping node and rolls the cycle so it begins with that
+    node. Returns an empty tuple if the cycle does not overlap with the path.
+    '''
+    rolled = None
+    # find the shared node that appears last in the path
+    for node in path[-1:0:-1]:
+        if node in cycle:
+            rolled = tuple(np.roll(cycle, -cycle.index(node)),)
+            break
+
+    return rolled
+
+
+def insert_cycles(pathway, rolled_cycles):
+    '''Inserts already-rolled cycles as a tuple into a path
+    '''
+    path=list(pathway)
+    for rc in rolled_cycles:
+        path.insert(path.index(rc[0]), rc)
+    path_with_cycles = tuple(path)
+    return tuple(path)
+
+
+def get_pathways_with_cycles(pathways, sc):
+    '''For the given list of pathways and simple cycles, returns a set of
+    pathways that could contain 1 or more cycles
+    '''
+    if len(pathways) is 0:
+        return set()
+
+    pathways_with_cycles = set()
+    for path in pathways:
+        rolled_cycles = set()
+        for cycle in sc:
+            rolled_cycle = roll_cycle(path, cycle)
+            if rolled_cycle:
+                rolled_cycles.add(rolled_cycle)
+        
+        # record all the pathways that have cycles, insert single cycle
+        if rolled_cycles:
+            pathways_with_cycles.add(insert_cycles(path, rolled_cycles))
+
+    return pathways_with_cycles
+
+
+def find_paths_with_source(pathways, source):
+    '''returns a subset of pathways that contain a given facility as the source
+    '''
+    if len(pathways) is 0:
+        return set()
+
+    subset_pathways = set([ path for path in pathways if path[0] == source])
+    return subset_pathways
+
+
+def find_paths_with_sink(pathways, sink):
+    '''returns a subset of pathways that contain a given facility as the sink
+    '''
+    if len(pathways) is 0:
+        return set()
+    
+    subset_pathways = set([ path for path in pathways if path[-1] == sink])
+    return subset_pathways
+
+
+def find_paths_containing_all(pathways, facilities):
+    '''returns a subset of pathways that contain all facilities in input list
+    '''
+    if len(pathways) is 0:
+        return set()
+
+    # convert to list if user passed a string or int
+    if type(facilities) == int or type(facilities) == str:
+        facilities = [facilities]
+    
+    # if user passed an empty list, return no pathways
+    if not facilities:
+        return set()
+
+    p = set([path for path in pathways if set(facilities).issubset(path)])
+
+    return p
+
+
+def find_paths_containing_one_of(pathways, facilities):
+    '''Returns a subset of pathways that contain one or more facilities in
+    input list
+    '''
+    # convert to list if user passed a string or int
+    if type(facilities) == int or type(facilities) == str:
+        facilities = [facilities]
+
+    # if user passed an empty list, return no pathways
+    if len(pathways) is 0 or len(facilities) is 0:
+        return set()
+
+    facilities = set(facilities)
+    p = set([path for path in pathways if set(path).intersection(facilities)])
+
+    return p
+
+
+def get_shortest_path(pathways):
+    '''Finds the set of pathways with the shortest number of steps from source to
+    target. Returns a tuple with path and length.
+    '''
+    if len(pathways) is not 0:
+        short_len = min([len(path) for path in pathways])
+        shortest = set([path for path in pathways if len(path) == short_len])
+    else:
+        shortest = set()
+
+    return shortest
+
+
+def get_longest_path(pathways):
+    '''Finds the pathway with the longest number of steps from source to
+    target. Returns a tuple with path and length.
+    '''
+    if len(pathways) is not 0:
+        long_len = max([len(path) for path in pathways])
+        longest = set([path for path in pathways if len(path) == long_len])
+    else:
+        longest = set()
+
+    return longest
+
+
+def get_sources(G):
+    '''Returns nodes that have no incoming edges.
+    '''
+    sources = list(node for node, in_deg in G.in_degree() if in_deg == 0)
+    return sources
+
+
+def get_sinks(G):
+    '''Returns nodes that have no outgoing edges.
+    '''
+    sinks = list(node for node, out_deg in G.out_degree() if out_deg == 0)
+    return sinks
